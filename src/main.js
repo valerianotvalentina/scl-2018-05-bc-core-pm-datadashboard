16945ac8581eeb27bf57a7ec4b6baeda87ff8ba7
Mi Nombre <valeriadroguettl@gmail.com>
2018-06-14 00:23:47 -0400

    probando

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 window.onload = () => {
     getNews();
-}
+};
 
 function getNews() { // q=${searchForText}
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "../data/cohorts.json", true);
+    xhr.open('GET', '../data/cohorts.json', true);
     xhr.onload = addNews;
     xhr.onerror = handleError;
     xhr.send();
@@ -12,19 +12,19 @@ function getNews() { // q=${searchForText}
 
 function addNews() {
     const data = JSON.parse(this.responseText);
-    console.log(data)
+    console.log(data);
     data.forEach(element => {
-        const id = element.id
+        const id = element.id;
         console.log(id);
     });
 }
 
 
 function handleError() {
-    console.log("Se ha presentado un error");
+    console.log('Se ha presentado un error');
 }
 
 function addNew() {
-    const data = JSON.users(this.responseText)
+    const data = JSON.users(this.responseText);
     console.log(data);
 }
