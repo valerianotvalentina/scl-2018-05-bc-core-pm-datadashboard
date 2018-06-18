6bdb9bd6610ec4ced945ee228b1b2ab64c8661c5
cynthia <cyntthiah@gmail.com>
2018-06-17 21:54:31 -0400

    json

--- src/data.js
+++ src/data.js
@@ -1,18 +1,21 @@
+function data() {
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+       // Typical action to be performed when the document is ready:
+       var users = JSON.parse(xhttp.responseText);
+       var nombres = "";
+       for(var i=0; i<users.length; i++)
+       {
+           nombres += users[i].name + ' ' + users[i].id+ '</br>';
+       }
+       document.getElementById("hola").innerHTML = nombres;
+    }
+};
+xhttp.open("GET","http://127.0.0.1:8887/data/cohorts/lim-2018-03-pre-core-pw/users.json", true);
+xhttp.send();
 
-var ciudades =
-{ciudad:santigo de chile;
-{ciudad:lima;
-{ciudad:ciudad de mexico;
-// aqui van los datos y cohors//
+};
 
-// funcion para obtener cohorts
-capitales.Getcohorts(){
-    for (var i = i < ciudades.lenght i++)//iteracion de info
-    ciudades [i] cohorts.lenght
-}
-ciudad[i] cohort.list
 
-//busqueda por ciudad//
-//busqueda por alumna (horario y generacion)
 
-//buscas utilizar map y filter
