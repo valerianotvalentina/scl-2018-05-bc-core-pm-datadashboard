9562569a999ccff40804a38c76cdb0105b959bff
Mi Nombre <valeriadroguettl@gmail.com>
2018-06-20 14:06:59 -0400

    javascript para index html

---
+++ src/indexnav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "277px";
+    document.getElementById("contenido").style.marginLeft = "277px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("contenido").style.marginLeft = "0";
+}
