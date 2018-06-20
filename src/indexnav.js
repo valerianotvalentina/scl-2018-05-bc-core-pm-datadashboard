858de409b9f07d4d5a70d3a338bedb070e48b650
Cecilia Cid <ccid.montero@gmail.com>
2018-06-20 15:05:54 -0400

    Separado de funciones del html

---
+++ src/indexnav.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "277px";
+    document.getElementById("contenido").style.marginLeft = "277px";
+    document.getElementById("contenidoDos").style.marginLeft = "277px";
+    document.getElementById("contenidoTres").style.marginLeft = "277px";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("contenido").style.marginLeft = "0";
+    document.getElementById("contenidoDos").style.marginLeft = "0";
+    document.getElementById("contenidoTres").style.marginLeft = "0";
+}
